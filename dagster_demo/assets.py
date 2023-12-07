d14db8bee7c6cd4da4f32b1d5ebef3d9c42f30af
Andreas Rjasanow <andrjas@gmail.com>
2023-12-07 18:40:06 +0100

    v1

--- dagster_demo/assets.py
+++ dagster_demo/assets.py
@@ -1,1 +1,13 @@
+import requests
+from pathlib import Path
+from dagster import asset, AssetExecutionContext, DailyPartitionsDefinition, MaterializeResult, define_asset_job, AssetSelection
 
+
+@asset
+def file_orders():
+    url = "https://raw.githubusercontent.com/dbt-labs/jaffle_shop/main/seeds/raw_orders.csv"
+    data = requests.get(url)
+    folder = Path("data/raw_orders")
+    folder.mkdir(parents=True, exist_ok=True)
+    path = folder / f"raw_orders.csv"
+    path.write_text(data.text)
