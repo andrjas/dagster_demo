fbaf474b3c6dff780d565f9ed2c6fec9e81226e6
Andreas Rjasanow <andrjas@gmail.com>
2023-12-07 18:46:42 +0100

    v5

--- dagster_demo/__init__.py
+++ dagster_demo/__init__.py
@@ -1,10 +1,14 @@
-from dagster import Definitions, load_assets_from_modules
+from dagster import Definitions, load_assets_from_modules, ScheduleDefinition
 
 from . import assets
 
 all_assets = load_assets_from_modules([assets])
 
+file_orders_and_orders_schedule = ScheduleDefinition(job=assets.asset_job, cron_schedule="0 0 * * *")
+file_orders_and_orders_partitioned_schedule = ScheduleDefinition(job=assets.partitioned_asset_job, cron_schedule="0 0 * * *")
+
 defs = Definitions(
     assets=all_assets,
     jobs=[assets.partitioned_asset_job],
+    schedules=[file_orders_and_orders_schedule, file_orders_and_orders_partitioned_schedule],
 )
